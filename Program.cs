a8f6896a5cd26a4e08831720fe1732f6b3194569
Santoshi Theja <100349070+SantoshiTeja@users.noreply.github.com>
2022-02-25 09:31:01 +0100

    c# sample program

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asyncexample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            fun1();
+            fun2();
+            string name ;
+            Console.WriteLine("Enter ur name");
+            name =  Console.ReadLine();
+            //Console.Read();
+
+
+        }
+        static async void fun1()
+        {
+            await Task.Delay(20000);
+            Console.WriteLine("Downloadind first file");
+        }
+        static async void fun2()
+        {
+            await Task.Delay(20000);
+            Console.WriteLine("Dowlaoding secound file");
+        }
+    }
+}
